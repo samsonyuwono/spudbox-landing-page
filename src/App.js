d26c3b6ed95125e4378a384dfa7904644121b551
github username <samson.yuwono@gmail.com>
2018-10-09 14:38:03 -0400

    Initial commit to heroku

--- src/App.js
+++ src/App.js
@@ -7,18 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello World!</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header className="App-header" />
       </div>
     );
   }
